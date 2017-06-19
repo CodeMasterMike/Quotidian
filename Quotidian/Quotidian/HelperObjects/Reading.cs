581add79077870eb209b046ddfae01cd69571c5d
Gabriel Wells <gmw24@pitt.edu>
2017-06-19 17:03:16 -0400

    Add linesPerPage and style attributes to Reading object:

--- Quotidian/Quotidian/HelperObjects/Reading.cs
+++ Quotidian/Quotidian/HelperObjects/Reading.cs
@@ -8,11 +8,14 @@ namespace Quotidian.HelperObjects
 {
     class Reading
     {
+        private readonly int LPP = 60;
         public int readingId { get; set; }
         public int projectId { get; set; }
         public String title { get; set; }
         public String author { get; set; }
         public String text { get; set; }
+        public int linesPerPage { get; set; }
+        public String style { get; set; }
 
         public Reading(int id, int pId, String t, String a, String txt)
         {
@@ -21,6 +24,12 @@ namespace Quotidian.HelperObjects
             title = t;
             author = a;
             text = txt;
+            linesPerPage = LPP;
+            style = "MLA";
         }
+
+        //MLA styling standard is currently 
+        //in line: [Author, pageNum]
+        //full citation: Last, First M. "Section Title." Book/Anthology. Ed. First M. Last. City: Publisher, Year Published. Page(s). Print.
     }
 }
