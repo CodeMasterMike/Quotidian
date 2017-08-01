1240d844577a8d53b9498d16b7213a092b999cfe
Gabriel Wells <gmw24@pitt.edu>
2017-08-01 15:17:17 -0400

    Minimize projectsummary instead of hide

--- Quotidian/Quotidian/ProjectSummary.cs
+++ Quotidian/Quotidian/ProjectSummary.cs
@@ -112,15 +112,20 @@ namespace Quotidian
             if (isNew == true)
             {
                 var nextPage = new ReadingInfo(currentProject, null, isNew);
-                this.Hide();
+                //this.Hide();
                 nextPage.Show();
+                this.WindowState = FormWindowState.Minimized;
             }
             else
             {
                 selectedReading = (Reading)readingsList.SelectedItem;
                 var nextPage = new ReadingPage(currentProject, selectedReading);
-                this.Hide();
+                //this.Hide();
+                //this.WindowState = FormWindowState.Minimized;
+                //nextPage.Show();
+                //nextPage.WindowState = FormWindowState.Normal;
                 nextPage.Show();
+                this.WindowState = FormWindowState.Minimized;
             }
         }
 
@@ -131,14 +136,16 @@ namespace Quotidian
                 Writing newWriting = new Writing(-1, currentProject.projectId, "Type here!");
                 currentProject.writings.Add(newWriting);
                 var nextPage = new WritingPage(currentProject, newWriting);
-                this.Hide();
+                //this.Hide();
                 nextPage.Show();
+                this.WindowState = FormWindowState.Minimized;
             }
             else
             {
                 var nextPage = new WritingPage(currentProject, selectedWriting);
-                this.Hide();
+                //this.Hide();
                 nextPage.Show();
+                this.WindowState = FormWindowState.Minimized;
             }
         }
 
